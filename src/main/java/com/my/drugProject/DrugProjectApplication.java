80fd3f2d59b5bd5ddd2ed4b045810f9b4549fe4d
zzzmini <sm01079200887@gmail.com>
2025-03-07 11:46:25 +0900

    first commit

---
+++ src/main/java/com/my/drugProject/DrugProjectApplication.java
@@ -0,0 +1,13 @@
+package com.my.drugProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/drugProject/DrugProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.drugProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
